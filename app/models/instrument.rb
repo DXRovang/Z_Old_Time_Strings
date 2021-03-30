e46ea7dda41bfad03f4dca1689f3d92e049d9458
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-30 14:37:02 -0400

    updates instrument model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -5,10 +5,9 @@ class Instrument < ApplicationRecord
 
   validates_presence_of :price, :year, :maker_name
  
-  def user_name=(name)
-    self.user = User.find_or_create_by(username: name)
+  def user_name=(name) #shouldn't need _or_create
+    self.user = User.find_by(username: name)
   end
-
   def user_name
     self.user ? self.user.username : nil
   end
@@ -16,7 +15,6 @@ class Instrument < ApplicationRecord
   def maker_name=(name)
     self.maker = Maker.find_or_create_by(name: name)
   end
-
   def maker_name
     self.maker ? self.maker.name : nil
   end
