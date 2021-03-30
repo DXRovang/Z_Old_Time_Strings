1f1c751d71be9b3d49bf17d69fd919b7c90690ff
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-30 17:59:12 -0400

    adds functionality to makers show

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,12 +1,12 @@
 class MakersController < ApplicationController
-  def index #currently useless
+  def index
     if params[:search]
       @makers = Maker.all.where("name = ?", params[:search])
       @makers = Maker.all.where("name LIKE ?", "%#{params[:search]}%")
     end
   end
 
-  def show #currently useless
+  def show
     @maker = Maker.find_by(id: params[:id])
   end
 
