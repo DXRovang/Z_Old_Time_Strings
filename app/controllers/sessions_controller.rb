c1bd6823b307dc6292f0637e702a315b1a57b41c
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-30 22:26:50 -0400

    deletes unnecessary links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # binding.pry
     @user = User.find_by(username: params[:username])
-    if @user 
-      #&& @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to users_path
+      redirect_to families_path
     else
       render :new
     end
