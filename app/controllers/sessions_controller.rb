45265a5ce89eef80e9c1f10b6606d7173026b778
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-31 16:23:24 -0400

    adds omniauth request.env to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,14 +17,17 @@ class SessionsController < ApplicationController
   end
 
   def create_with_fb
-    user = User.find_or_create_by(username: self.request.env['ominauth.auth']['info']['email']) do |u| 
-      u.password = 'password'
+
+    user = User.find_or_create_by(
+      username: self.request.env['omniauth.auth']['info']['name'],
+      email: self.request.env['omniauth.auth']['info']['email']) do |u| 
+        u.password = 'password'
     end
     if user.save
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      render :new
+      redirect_to signup_path
     end
   end
   
