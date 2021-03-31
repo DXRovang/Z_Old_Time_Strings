699ffd6176ec18d7b4a95a89efcd41ca95d7de38
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-30 22:43:05 -0400

    cleans up code formatting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to families_path
-    else
+    if params[:username] == "" || params[:password] == "" || params[:confirm_password] == ""
+      @error = "You must fill in all fields correctly"
       render :new
+    else
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to families_path
+      else
+        render :new
+      end
     end
   end
 
