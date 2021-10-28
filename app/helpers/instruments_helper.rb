09f82e3121d6ec59dafb4260326493bc0103aa19
Alexander Rovang <DXRovang@gmail.com>
2021-10-27 22:15:19 -0400

    debugs category swap

--- app/helpers/instruments_helper.rb
+++ app/helpers/instruments_helper.rb
@@ -7,11 +7,11 @@ module InstrumentsHelper
     Category.where(id: [1...5])
   end
 
-  def mandolin_categories
+  def guitar_categories
     Category.where(id: [5...13])
   end
 
-  def guitar_categories
+  def mandolin_categories
     Category.where(id: [13...16])
   end
 
